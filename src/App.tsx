b792f77c2fdf4997ae545786f783a3dc41129bae
hokuto-kato <hokuto15@gmail.com>
2022-08-09 13:28:20 +0900

    routing adjustment

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,10 @@ import { BrowserRouter, HashRouter } from "react-router-dom"
 import { theme } from "./theme/theme"
 import { Router } from "./router/Router"
 
-const base = process.env.NODE_ENV === "development" ? "/" : "/userManagement_react"
-
 export const App = () => {
 	return (
 		<ChakraProvider theme={theme}>
-			<HashRouter basename={base}>
+			<HashRouter>
 				<Router />
 			</HashRouter>
 		</ChakraProvider>
